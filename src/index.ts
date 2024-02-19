7a7bc06c39d0d783c17f696f5c49b2cf55c466b2
Rocca <arnaud@toosoon.paris>
2024-02-19 21:45:03 +0100

    release: node-next module

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export { BRANCH_SYMBOLS, PARAMETRIC_SYMBOLS, IGNORED_SYMBOLS } from './symbols';
 // export type { TurtleParameters } from './turtle';
 // export { default as Turtle } from './turtle';
 
-export type * from './types';
-
-export type { LSystemParameters } from './lsystem';
 export { default } from './lsystem';
+export type { LSystemParameters } from './lsystem';
+export type * from './types';
